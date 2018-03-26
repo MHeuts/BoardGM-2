15b349220d7b504ba87ad18c17d3ba778deafea9
Marijn Heuts <m.heuts@live.nl>
2018-03-26 11:28:45 +0200

    Started Routing structure

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['namespace' => 'frontend', 'as' => 'frontend.'], function(){
+        Route::get('/', ['as' => 'home.index', 'uses' => 'HomeController@index']);
+});
