ca2dd87bf6ae444ae0a06e7e095303feaa595c85
Marijn Heuts <m.heuts@live.nl>
2018-03-27 11:31:10 +0200

    created product page layout

--- resources/views/frontend/layout/main.blade.php
+++ resources/views/frontend/layout/main.blade.php
@@ -1,6 +1,7 @@
 <html>
 <head>
-    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{asset('css/front.css')}}" rel="stylesheet">
 </head>
 <body>
 <div id="Head" style="background-color: #287ccf; height: 150px; padding: 0px" >
@@ -9,7 +10,7 @@
     </div>
 
 </div>
-<div id="content" style="width: 1000px; margin: auto">
+<div id="content">
     @yield('content')
 </div>
 
--- resources/views/frontend/product/detail.blade.php
+++ resources/views/frontend/product/detail.blade.php
@@ -1,15 +1,42 @@
 @extends('frontend.layout.main')
 
 @section('content')
-        <h1>ProductName</h1>
-        <div>
-            <div class="product_image" style="height: 150px; width: 150px;">
-                <img class="product_image_image" style="height: 450px; width: 450px;" src="{{ asset('images/product/test/1.jpg') }}"/>
-            </div>
-            <div>
-                <h2>prijs</h2>
-                <p>prduct omschrijving komt hier</p>
+    <div class="container">
+        <div class="product_page">
+            <h1>Battlestar Galactica</h1>
+            <div class="row">
+                <div class="col-sm-6">
+                    <img class="product_image_image" src="{{ asset('images/product/test/1.jpg') }}"/>
+                </div>
+                <div class="col-sm-6">
+                    <h2>49,99</h2>
+                    <p>prduct omschrijving komt hier: \n
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                    <button type="button" class="btn btn-primary">Add to Basket</button>
+                </div>
             </div>
 
         </div>
+        <div class="product_reviews">
+            <div class="Review_container">
+                <div class="review_header">
+                    <div class="row">
+                        <div class="col-sm-10">
+                            <h3>Review Title</h3>
+                            <p>review Naam</p>
+                        </div>
+                        <div class="col-sm-2">
+                            STARS
+                        </div>
+                    </div>
+
+                </div>
+                <div class="review_body">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+        </div>
+    </div>
 @endsection
