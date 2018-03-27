2f9f59984c5046ac1aa618176db95e6b051fc63a
Marijn Heuts <m.heuts@live.nl>
2018-03-27 09:59:22 +0200

    added main layout file

---
+++ resources/views/frontend/layout/main.blade.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+
+</head>
+<body>
+<div id="Head" style="background-color: #287ccf; height: 150px; padding: 0px" >
+    <div id="HeadContainer" style="width: 1000px; margin: auto">
+        <img src="{{ asset('images/main/Logo.png') }}"/>
+    </div>
+
+</div>
+<div id="content" style="width: 1000px; margin: auto">
+    @yield('content')
+</div>
+
+
+
+</body>
+</html>
