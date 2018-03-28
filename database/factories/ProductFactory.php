e07ee46955a4e724b48a5f7381d876e7a86d0da6
Tom Verstappen <tthverst@avans.nl>
2018-03-28 19:08:59 +0200

    fix price datatype

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -7,6 +7,6 @@ $factory->define(Product::class, function (Faker $faker) {
     return [
         'name' => $faker->words($nb = 3, $asText = true),
 		'description' => $faker->realText($maxNbChars = 200, $indexSize = 2),
-		'price' => $faker->randomNumber(2),
+		'price' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 900),
     ];
 });
