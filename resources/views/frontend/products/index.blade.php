b3201b3a7d4302edf93018185965e296f929d915
Tom Verstappen <tthverst@avans.nl>
2018-03-29 20:10:35 +0200

    Card markup changed

--- resources/views/frontend/products/index.blade.php
+++ resources/views/frontend/products/index.blade.php
@@ -7,12 +7,19 @@
 		@foreach ($products as $product)
 			<div class="col-lg-2 col-md-4 col-sm-6 mb-4">
 				<div class="card">
+					<h5 class="card-header">{{ $product->name }}</h5>
 					<img class="card-img-top" src="{{ asset("images/product/{$product->id}/1.jpg") }}" alt="Card image">
 					{{-- <img class="card-img-top" src="http://via.placeholder.com/800x800" alt="Card image"> --}}
 					<div class="card-body">
-						<h4 class="card-title">{{ $product->name }}</h4>
 						<p class="card-text text-truncate">{{ $product->description }}</p>
 						<a href="{{ route('displayProduct', $product->id) }}" class="btn btn-primary">View product</a>
+					</div>
+					<div class="card-footer text-muted">
+						<i class="fa fa-star text-warning"></i>
+						<i class="fa fa-star text-warning"></i>
+						<i class="fa fa-star text-warning"></i>
+						<i class="fa fa-star-half-o text-warning"></i>
+						<i class="fa fa-star-o text-warning"></i>
 					</div>
 				</div>
 			</div>
