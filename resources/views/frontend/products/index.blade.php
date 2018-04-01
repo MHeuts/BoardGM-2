1be433d807db5367244ba9fb6f26ae1d91858dba
Tom Verstappen <tthverst@avans.nl>
2018-04-01 12:34:24 +0200

    Layout fix

--- resources/views/frontend/products/index.blade.php
+++ resources/views/frontend/products/index.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-	<div class="container">
-		<h1 class="display-3 text-center">Products</h1>
-		
-		<div class="container">
-			<div class="row pull-left mr-4 d-none d-md-block">
+	<h1 class="display-3 text-center">Products</h1>
+	
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-2">
 				<div class="card">
 					<h5 class="card-header">Categories</h5>
 					<div class="card-body">
@@ -32,28 +32,29 @@
 					</div>
 				</div>
 			</div>
-		</div>
-		
-		<div class="container-fluid">
-			<div class="row">
-			@foreach ($products as $product)
-				<div class="col-lg-4 col-md-6 col-sm-12 mb-4">
-					<a href="{{ route('displayProduct', $product->id) }}">
-					<div class="card">
-						<h5 class="card-header">{{ $product->name }}</h5>
-						<img class="card-img-top" src="{{ asset("images/product/{$product->id}/1.jpg") }}" alt="Card image">
-						{{-- <img class="card-img-top" src="http://via.placeholder.com/800x800" alt="Card image"> --}}
-						<div class="card-body">
-							<p class="card-text text-truncate">{{ $product->description }}</p>
+			<div class="col-lg-10">
+				<div class="container-fluid">
+					<div class="row">
+						@foreach ($products as $product)
+						<div class="col-lg-4 col-md-6 col-sm-12 mb-4">
+							<a href="{{ route('displayProduct', $product->id) }}">
+							<div class="card">
+								<h5 class="card-header">{{ $product->name }}</h5>
+								<img class="card-img-top" src="{{ asset("images/product/{$product->id}/1.jpg") }}" alt="Card image">
+								{{-- <img class="card-img-top" src="http://via.placeholder.com/800x800" alt="Card image"> --}}
+								<div class="card-body">
+									<p class="card-text text-truncate">{{ $product->description }}</p>
+								</div>
+								<div class="card-footer text-muted">
+									<a href="{{ route('displayProduct', $product->id) }}" class="btn btn-sm btn-primary"><i class="fa fa-eye"></i> View product</a>
+									<a href="{{ route('addToCart', $product->id) }}" class="btn btn-sm btn-primary"><i class="fa fa-cart-plus"></i> Add to cart</a>
+								</div>
+							</div>
+							</a>
 						</div>
-						<div class="card-footer text-muted">
-							<a href="{{ route('displayProduct', $product->id) }}" class="btn btn-sm btn-primary"><i class="fa fa-eye"></i> View product</a>
-							<a href="{{ route('addToCart', $product->id) }}" class="btn btn-sm btn-primary"><i class="fa fa-cart-plus"></i> Add to cart</a>
-						</div>
+						@endforeach
 					</div>
-					</a>
 				</div>
-				@endforeach
 			</div>
 		</div>
 	</div>
