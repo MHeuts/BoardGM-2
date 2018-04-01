754f20f773d4fcc40292059a7afab1d580391292
Tom Verstappen <tthverst@avans.nl>
2018-04-01 14:29:20 +0200

    Show no resulst found message

--- resources/views/frontend/search/index.blade.php
+++ resources/views/frontend/search/index.blade.php
@@ -6,32 +6,36 @@
 		
 		<table class="table table-striped">
 			<tbody>
-				@foreach ($products as $product)
-				<tr>
-					<th scope="row">
-						<img class="search-product-img" src="{{ asset("images/product/{$product->id}/1.jpg") }}"/>
-					</th>
-					<td>
-						<a href="{{ route('displayProduct', $product->id) }}">
-							{{ $product->name }}
-						</a>
-					</td>
-					<td>
-						€ {{ $product->price }}
-					</td>
-					<td>
-						<i class="fa fa-star text-warning"></i>
-						<i class="fa fa-star text-warning"></i>
-						<i class="fa fa-star text-warning"></i>
-						<i class="fa fa-star-half-o text-warning"></i>
-						<i class="fa fa-star-o text-warning"></i>
-					</td>
-					<td>
-						<a href="{{ route('displayProduct', $product->id) }}" class="btn btn-primary mr-2"><i class="fa fa-eye"></i> View product</a>
-						<a href="{{ route('addToCart', $product->id) }}" class="btn btn-primary"><i class="fa fa-cart-plus"></i> Add to cart</a>
-					</td>
-				</tr>
-				@endforeach
+				@if(count($products) > 0)
+					@foreach ($products as $product)
+					<tr>
+						<th scope="row">
+							<img class="search-product-img" src="{{ asset("images/product/{$product->id}/1.jpg") }}"/>
+						</th>
+						<td>
+							<a href="{{ route('displayProduct', $product->id) }}">
+								{{ $product->name }}
+							</a>
+						</td>
+						<td>
+							€ {{ $product->price }}
+						</td>
+						<td>
+							<i class="fa fa-star text-warning"></i>
+							<i class="fa fa-star text-warning"></i>
+							<i class="fa fa-star text-warning"></i>
+							<i class="fa fa-star-half-o text-warning"></i>
+							<i class="fa fa-star-o text-warning"></i>
+						</td>
+						<td>
+							<a href="{{ route('displayProduct', $product->id) }}" class="btn btn-primary mr-2"><i class="fa fa-eye"></i> View product</a>
+							<a href="{{ route('addToCart', $product->id) }}" class="btn btn-primary"><i class="fa fa-cart-plus"></i> Add to cart</a>
+						</td>
+					</tr>
+					@endforeach
+				@else
+						<h3 class="text-center">No results found.</h3>
+				@endif
 			</tbody>
 		</table>
 		
